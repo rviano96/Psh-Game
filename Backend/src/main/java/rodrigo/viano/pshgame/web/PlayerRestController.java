75c1543cc9602205aa1cac76f3b857435d91e5b8
Rodrigo Viano <rodrigo.viano@naranja.com>
2021-06-28 19:31:20 -0300

    Added cross origin support for localhost

--- Backend/src/main/java/rodrigo/viano/pshgame/web/PlayerRestController.java
+++ Backend/src/main/java/rodrigo/viano/pshgame/web/PlayerRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import rodrigo.viano.pshgame.business.IPlayerBusiness;
 import rodrigo.viano.pshgame.model.dto.PlayerDto;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(Constantes.URL_BASE_PLAYERS)
 public class PlayerRestController {
     @Autowired
